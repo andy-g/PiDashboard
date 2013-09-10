9c2be082bc7a6f0a3579cc7fb7f7f23566f1402b
Andy Gardner <andyg80@gmail.com>
2013-09-10 17:39:24 +0200

    Removing legacy methods and cleaning up code

--- app.js
+++ app.js
@@ -19,8 +19,6 @@ app.use(express.static(__dirname + '/public'));
 //-----routes-----
 app.get('/', function(req, res){ res.send('hello world'); });
 
-app.get('/save', usage.saveUsage);
-app.get('/today', usage.todaysUsage);
 app.get('/summary/:runDate?', usage.usageSummary);
 app.get('/drives', usage.drives);
 app.get('/services/:service?/:status?', usage.serviceStatus);
--- routes/usage.js
+++ routes/usage.js
@@ -9,56 +9,6 @@ var urlparser = require('url'),
 //cache usage snapsot at the start of today
 var _dayStartUsage;
 
-exports.saveUsage = function(req, res) {
-    res.header("Access-Control-Allow-Origin", "*");
-	GetTodaysUsage(function (err, today_usage) {
-		if (today_usage) 
-			res.json(500, {"err" : "Usage data already exists for today"}); 
-		else{	
-			GetCurrentUsage(function(err, current_usage){
-				if (err) { 
-					console.log("save error: " + JSON.stringify(err));
-					res.json(500, { err: "Error saving usage data." });
-				} else {
-					SaveUsageHistory(current_usage, function(message){
-						res.json(message);
-					});
-				}
-			});
-		}
-	});
-};
-
-exports.todaysUsage = function(req, res) {
-	res.header("Access-Control-Allow-Origin", "*");
-	GetCurrentUsage(function (err, current_usage) {
-		GetTodaysUsage(function(err,today_usage){
-			//Iterate through current usage stats
-			if (!current_usage){
-				res.json(500, {"err" : "Usage data could not be retrieved."});
-				return;
-			}
-			current_usage.stats.forEach(function(value, index) {
-				//Determine Today's usage for this device
-				var today_device_usage = "-";
-				if (today_usage){
-					var filtered_device = today_usage.stats.filter(function (element, index, array) {  return (element.ip_add === value.ip_add); });
-					if (filtered_device.length > 0 && parseInt(filtered_device[0].total_bytes) <= parseInt(value.total_bytes))
-						today_device_usage = value.total_bytes - filtered_device[0].total_bytes;
-					else
-     					today_device_usage = value.total_bytes;
-				}
-				else
-     					today_device_usage = value.total_bytes;
-				value.today_bytes = today_device_usage;
-			});
-			current_usage.stats.sort(function(a,b){ return b.today_bytes - a.today_bytes; });
-			res.type('json');
-			res.json(current_usage);	
-		});
-	});
-};
-
 //try cache data, and load it on startup or when rewriting to the file
 exports.usageSummary = function(req, res) {
 	var runDate = !req.param('runDate') ? new Date() : new Date(parseInt(req.param('runDate')));
@@ -69,83 +19,70 @@ exports.usageSummary = function(req, res) {
 			current_usage.isCurrent = true;
 			data = data.concat(current_usage);
 
-			if(err || !data){ 
-				console.log( { msg: "GetTodaysUsage: No data found for today.", "error": err } );
-			} else {
-				//sort by date
-				data.sort(function(a,b){ return a.date - b.date; });
-				data.totals = {};
+			//sort by date
+			data.sort(function(a,b){ return a.date - b.date; });
+			data.totals = {};
 
-				//process each period
-				data.reduce(function(previousValue, currentValue, index, array){
-			  		var jobPeriod = _settings.timePeriods.
-			  			filter(function (element, index, array) {  
-			  				if (currentValue.isCurrent){ //Current usage should always be logged against the next period
-			  					return (element.endHour > (new Date(currentValue.date)).getHours()); 
-			  				}
-			  				else
-			  					return (element.endHour >= (new Date(currentValue.date)).getHours()); 
-			  			}).
-						sort(function(a,b){ return a.endHour - b.endHour; })[0];
-					var periodDate = new Date(currentValue.date);
+			//process each period
+			data.reduce(function(previousValue, currentValue, index, array){
+		  		var periodUsage = 0;
+		  		var jobPeriod = _settings.timePeriods.filter(function (element, index, array) {  
+						return currentValue.isCurrent ? 
+							element.endHour > (new Date(currentValue.date)).getHours() :
+							element.endHour >= (new Date(currentValue.date)).getHours();
+		  			}).sort(function(a,b){ return a.endHour - b.endHour; })[0];
+				var periodDate = new Date(currentValue.date);
 
-					//process each ip address for period
-			  		currentValue.stats.reduce(function(previousValue, currentValue, index, array){
-			  			if (!data.totals[currentValue.ip_add])
-			  				data.totals[currentValue.ip_add] = { usageToDate: { total: 0 }, usageToday: { total: 0 }, lastTotal: 0 };
-			  				//data.totals[currentValue.ip_add] = { usageToDate: {}, usageToday: {}, lastTotal: 0 };
+				//process each ip address for period
+		  		currentValue.stats.reduce(function(previousValue, currentValue, index, array){
+		  			if (!data.totals[currentValue.ip_add])
+		  				data.totals[currentValue.ip_add] = { usageToDate: { total: 0 }, usageToday: { total: 0 }, lastTotal: 0 };
 
-			  			data.totals[currentValue.ip_add].mac_add = currentValue.mac_add;
-			  			data.totals[currentValue.ip_add].device_name = currentValue.device_name;
+		  			data.totals[currentValue.ip_add].mac_add = currentValue.mac_add;
+		  			data.totals[currentValue.ip_add].device_name = currentValue.device_name;
 
-						//If we're starting a new month, period usage should be forced to 0
-						var periodUsage = 0;
-			  			if (periodDate.getDate() != 1 || periodDate.getHours() != 0){
-							periodUsage = parseInt(currentValue.total_bytes) - (data.totals[currentValue.ip_add].lastTotal || 0);
-							if (periodUsage < 0) { //if periodUsage is < 0, router must have been reset in the period, so just use the Total usage as a period usage
-								periodUsage = parseInt(currentValue.total_bytes);
-							}
-			  			}
+					//If we're starting a new month, period usage should be forced to 0
+					if (periodDate.getDate() != 1 || periodDate.getHours() != 0){
+						periodUsage = parseInt(currentValue.total_bytes) - (data.totals[currentValue.ip_add].lastTotal || 0);
+						if (periodUsage < 0) { //if periodUsage is < 0, router must have been reset in the period, so just use the Total usage as a period usage
+							periodUsage = parseInt(currentValue.total_bytes);
+						}
+		  			}
 
-			  			data.totals[currentValue.ip_add].usageToDate[jobPeriod.name] = (data.totals[currentValue.ip_add].usageToDate[jobPeriod.name] || 0) + periodUsage;
-			  			data.totals[currentValue.ip_add].usageToDate.total = data.totals[currentValue.ip_add].usageToDate.total + periodUsage;
-			  			data.totals[currentValue.ip_add].lastTotal = parseInt(currentValue.total_bytes);
+		  			data.totals[currentValue.ip_add].usageToDate[jobPeriod.name] = (data.totals[currentValue.ip_add].usageToDate[jobPeriod.name] || 0) + periodUsage;
+		  			data.totals[currentValue.ip_add].usageToDate.total = data.totals[currentValue.ip_add].usageToDate.total + periodUsage;
+		  			data.totals[currentValue.ip_add].lastTotal = parseInt(currentValue.total_bytes);
 
-						//Get Today's (runDate) usage (only include if period date is after 00:01 - allow 1 minute delay in midnight run running)
-						if (periodDate > new Date(runDate).setHours(0,1,0,0) && periodDate <= new Date(new Date(runDate).setDate(runDate.getDate()+1)).setHours(0,1,0,0)){
-			  				data.totals[currentValue.ip_add].usageToday[jobPeriod.name] = (data.totals[currentValue.ip_add].usageToday[jobPeriod.name] || 0) + periodUsage;
-			  				data.totals[currentValue.ip_add].usageToday.total = data.totals[currentValue.ip_add].usageToday.total + periodUsage;
-			  			}
-			  		},0);
-				},0);
+					//Get Today's (runDate) usage (only include if period date is after 00:01 - allow 1 minute delay in midnight run running)
+					if (periodDate > new Date(runDate).setHours(0,1,0,0) && periodDate <= new Date(new Date(runDate).setDate(runDate.getDate()+1)).setHours(0,1,0,0)){
+		  				data.totals[currentValue.ip_add].usageToday[jobPeriod.name] = (data.totals[currentValue.ip_add].usageToday[jobPeriod.name] || 0) + periodUsage;
+		  				data.totals[currentValue.ip_add].usageToday.total = data.totals[currentValue.ip_add].usageToday.total + periodUsage;
+		  			}
+		  		},0);
+			},0);
 
-				//enrich and format
-				data.output = { date: Date.parse(runDate), stats: [] };
-				for (var prop in data.totals) {
-					delete data.totals[prop].lastTotal;
-					data.output.stats.push({
-						"ip_add":prop, 
-						"mac_add":data.totals[prop].mac_add, 
-						"device_name":data.totals[prop].device_name, 
-						"total_bytes": data.totals[prop].usageToDate.total,	//include for backwards compatibility
-						"today_bytes": data.totals[prop].usageToday.total,	//include for backwards compatibility
-						usageToDate:data.totals[prop].usageToDate, 
-						usageToday:data.totals[prop].usageToday });
-				}
-				
-				res.type('json');
-				data.output.stats.sort(function(a,b){ 
-					if (b.usageToday.total != a.usageToday.total) {
-						return b.usageToday.total - a.usageToday.total;
-					} else {
-						return b.usageToDate.total - a.usageToDate.total;
-					} 
-				});
-				data.output.stats = data.output.stats.filter(function(element, index, array){ 
-					return element.usageToDate.total != 0;	
-				});
-				res.json(data.output);
+			//enrich and format
+			data.output = { date: Date.parse(runDate), stats: [] };
+			for (var prop in data.totals) {
+				delete data.totals[prop].lastTotal;
+				data.output.stats.push({
+					"ip_add": prop, 
+					"mac_add": data.totals[prop].mac_add, 
+					"device_name": data.totals[prop].device_name, 
+					"total_bytes": data.totals[prop].usageToDate.total,	//include for backwards compatibility
+					"today_bytes": data.totals[prop].usageToday.total,	//include for backwards compatibility
+					usageToDate: data.totals[prop].usageToDate, 
+					usageToday: data.totals[prop].usageToday });
 			}
+			
+			res.type('json');
+			data.output.stats.sort(function(a,b){ 
+				return b.usageToday.total != a.usageToday.total ? b.usageToday.total - a.usageToday.total :	b.usageToDate.total - a.usageToDate.total;
+			});
+			data.output.stats = data.output.stats.filter(function(element, index, array){ 
+				return element.usageToDate.total != 0;	
+			});
+			res.json(data.output);
 		});
 	});
 };
@@ -165,12 +102,7 @@ exports.drives = function(req, res) {
     				res.json(drive_usage);
 	    	})
 	});
-
 };
-
-// exports.serviceStatus = function(req, res) {
-//     var id = req.params.id;
-// };
 
 exports.serviceStatus = function(req, res) {
 	res.header("Access-Control-Allow-Origin", "*");
@@ -229,29 +161,6 @@ function ExecService(serviceName, status, callback){
 		if (callback)
 			callback({ service: serviceName, status: (status == "status" ? (stdout.indexOf(serviceName + " is running") > -1) : (status == "start")) });
 	});
-}
-
-//Load historic stats & filter to find today's record
-function GetTodaysUsage(callback){
-	if (_dayStartUsage && (new Date(_dayStartUsage.date).setHours(0,0,0,0) === new Date(Date.now()).setHours(0,0,0,0))){
-		callback(undefined, _dayStartUsage);
-	} else {
-		GetUsageHistory(function (err, data) {
-			if(err || !data){ 
-				callback( { msg: "GetTodaysUsage: No data found for today.", "error": err } );
-			}else{		
-				var filtered = data.filter(function (element, index, array) {  return (new Date(element.date).setHours(0,0,0,0) === new Date(Date.now()).setHours(0,0,0,0)); });
-				if (filtered.length > 0){ 
-					_dayStartUsage = filtered[0];
-					callback(err,_dayStartUsage);
-				}
-				else{
-					callback({ msg: "GetTodaysUsage: No data found for today.", "error": null });
-					return;
-				}
-			}
-		});
-	}
 }
 
 function GetUsageHistory(callback){
