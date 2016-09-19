075882c68935c1deae1b52e9bf88d6ffbf89bf9d
Andy Gardner <andyg@mukuru.com>
2016-09-19 20:27:32 +0200

    Adding JWT authentication

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-var	express = require('express'),
-	fs = require('fs'),
-	request = require('request'),
-	schedule = require('node-schedule'),
-	routes = require('./routes'),
-	appSettings = require('./app.config.json'),
-	System = require('./routes/system');
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    compression = require('compression'),
+    cookieParser = require('cookie-parser'),
+    fs = require('fs'),
+    schedule = require('node-schedule'),
+    routes = require('./routes'),
+    appSettings = require('./app.config.json'),
+    System = require('./routes/system');
 
 //Prevent the application from crashing due to any unhandled exceptions
 process.on('uncaughtException', function(err) {
-	console.error(new Date().toJSON() + ' unhandled exception!', err);
-	console.log(err.stack);
+    console.error(new Date().toJSON() + ' unhandled exception!', err);
+    console.log(err.stack);
 });
 
 var app = express();
-//app.use(express.cookieParser());
-//app.use(express.session({ secret: 'foobar' }));
-app.use(express.compress());
-app.use(express.urlencoded());
-app.use(express.json());
+
+app.set('jwtSecret', appSettings.jwtSecret);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
 var system = new System(appSettings);
@@ -39,57 +43,56 @@ system.log('Listening on 443');
 var rule = new schedule.RecurrenceRule();
 rule.minute = 0;
 
-var j = schedule.scheduleJob('network usage log',rule, function(){
-	// If were catching up jobs (due to a datetime change), skip previous jobs & only run the last job
-	if (j.nextInvocation() < new Date())
-	{
-		system.log('skipping job');
-		return;
-	}
+var j = schedule.scheduleJob('network usage log', rule, function() {
+    // If were catching up jobs (due to a datetime change), skip previous jobs & only run the last job
+    if (j.nextInvocation() < new Date()) {
+        system.log('skipping job');
+        return;
+    }
 
-	system.log("Retrieving hourly usage: ");
-	system.saveCurrentNetworkUsage(function(message){
-		system.log(message);
-	});
+    system.log("Retrieving hourly usage: ");
+    system.saveCurrentNetworkUsage(function(message) {
+        system.log(message);
+    });
 });
 system.log('Scheduled hourly job for logging usage');
 
 //-----listen for direct messages
-if (appSettings.twitter.enableTwitterBot){
-	var TwitterBot = require('./routes/twitterBot');
-	var twitterBot = new TwitterBot(appSettings);
+if (appSettings.twitter.enableTwitterBot) {
+    var TwitterBot = require('./routes/twitterBot');
+    var twitterBot = new TwitterBot(appSettings);
 
-	twitterBot.on('tweet',function(data){
-		system.log('Tweet event received');
-		if (data.direct_message && data.direct_message.sender_screen_name != 'PiTweetBot'){
-			system.log('New tweet event handler: ' + data.direct_message.text);
-			
-			//--Retrieve Current public IP Address
-			if (data.direct_message.text.match(/IP Address/gi) !== null){
-				system.getIpAddress(function(err, ipAddress){
-					if (err){
-						twitterBot.SendDirectMessage('IP address could not be retrieved.');
-					} else {
-						twitterBot.SendDirectMessage('My IP Address is ' + ipAddress, data.direct_message.sender_screen_name);
-					}
-				});
-			} 
-		}
-	});
-	twitterBot.StartTwitterListener();
+    twitterBot.on('tweet', function(data) {
+        system.log('Tweet event received');
+        if (data.direct_message && data.direct_message.sender_screen_name != 'PiTweetBot') {
+            system.log('New tweet event handler: ' + data.direct_message.text);
+
+            //--Retrieve Current public IP Address
+            if (data.direct_message.text.match(/IP Address/gi) !== null) {
+                system.getIpAddress(function(err, ipAddress) {
+                    if (err) {
+                        twitterBot.SendDirectMessage('IP address could not be retrieved.');
+                    } else {
+                        twitterBot.SendDirectMessage('My IP Address is ' + ipAddress, data.direct_message.sender_screen_name);
+                    }
+                });
+            }
+        }
+    });
+    twitterBot.StartTwitterListener();
 }
 
 //Send notifications that the PiDashboard is now up
-system.getIpAddress(function(err, ipAddress){
-	if (err){
-		system.sendPushNotification('PiDashboard is up! IP address could not be retrieved.');
-		if (appSettings.twitter.enableTwitterBot){
-			twitterBot.SendDirectMessage('PiDashboard is up! IP address could not be retrieved.');
-		}
-	} else {
-		system.sendPushNotification('PiDashboard is up! IP Address is ' + ipAddress);
-		if (appSettings.twitter.enableTwitterBot){
-			twitterBot.SendDirectMessage('PiDashboard is up! IP Address is ' + ipAddress);
-		}
-	}
+system.getIpAddress(function(err, ipAddress) {
+    if (err) {
+        system.sendPushNotification('PiDashboard is up! IP address could not be retrieved.');
+        if (appSettings.twitter.enableTwitterBot) {
+            twitterBot.SendDirectMessage('PiDashboard is up! IP address could not be retrieved.');
+        }
+    } else {
+        system.sendPushNotification('PiDashboard is up! IP Address is ' + ipAddress);
+        if (appSettings.twitter.enableTwitterBot) {
+            twitterBot.SendDirectMessage('PiDashboard is up! IP Address is ' + ipAddress);
+        }
+    }
 });
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,59 @@
-var ContentHandler = require('./usage');
+var ContentHandler = require('./usage'),
+    jwt = require('jsonwebtoken');
 
 module.exports = exports = function(app, appSettings) {
-	var contentHandler = new ContentHandler(appSettings);
+    var contentHandler = new ContentHandler(appSettings);
 
-	// Middleware to see if a user is logged in
-	//app.use(sessionHandler.isLoggedInMiddleware);
+    app.get('/', function(req, res) { res.send('hello world'); });
 
-	app.get('/', function(req, res){ res.send('hello world'); });
-	app.get('/summary/:runDate?', contentHandler.usageSummary);
-	app.get('/graph_data/:runDate?', contentHandler.graphData);
-	app.get('/drives', contentHandler.drives);
-	app.get('/services/:service?/:status?', contentHandler.serviceStatus);
-	app.put('/services/:service?/:status?', contentHandler.serviceStatus);
-	app.get('/devices/:device?/:status?', contentHandler.deviceStatus);
-	app.put('/devices/:device?/:status?', contentHandler.deviceStatus);
+    app.post('/login', function(req, res) {
+        var user = {
+            name: 'andyg',
+            password: 'roam.home',
+            admin: true
+        };
 
-	// Error handling middleware
-	//app.use(ErrorHandler);
+        if (req.body.name != user.name || req.body.password != user.password) {
+            res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+        } else {
+            delete user.password;
+            var token = jwt.sign(user, app.get('jwtSecret'), {
+                expiresIn: "30 days"
+            });
+            res.cookie('access_token', token, { maxAge: 900000, httpOnly: true });
+            res.json({
+                success: true,
+                token: token
+            });
+        }
+    });
+
+    // route middleware to verify a token
+    app.use(function(req, res, next) {
+        var token = req.cookies.access_token || req.body.token || req.query.token || req.headers['x-access-token'];
+        if (token) {
+            // verifies secret and checks exp
+            jwt.verify(token, app.get('jwtSecret'), function(err, decoded) {
+                if (err) {
+                    return res.json({ success: false, message: 'Failed to authenticate token.' });
+                } else {
+                    req.decoded = decoded;
+                    next();
+                }
+            });
+        } else {
+            return res.status(403).send({
+                success: false,
+                message: 'No token provided.'
+            });
+        }
+    });
+
+    app.get('/summary/:runDate?', contentHandler.usageSummary);
+    app.get('/graph_data/:runDate?', contentHandler.graphData);
+    app.get('/drives', contentHandler.drives);
+    app.get('/services/:service?/:status?', contentHandler.serviceStatus);
+    app.put('/services/:service?/:status?', contentHandler.serviceStatus);
+    app.get('/devices/:device?/:status?', contentHandler.deviceStatus);
+    app.put('/devices/:device?/:status?', contentHandler.deviceStatus);
 };
